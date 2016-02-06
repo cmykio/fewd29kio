d6165937ba93a65d8de4f81c06d6bf32a1b3fc93
cmykio <hello@cmykio.com>
2016-02-06 01:12:10 +0000

    Main path styling complete for lowest breakpoint
    just need to recolour quiz now.

--- js/main.js
+++ js/main.js
@@ -1,81 +1,12 @@
 
-
-
-
-// Responsive Navigation and slider - just getting it functional for now
 $(document).ready(function(){
-
-     
-	$('.nav-icon').on('click', function(){
-		$('.site-nav ul').slideToggle();
-		// this slideToggle is part of the effect library in jQuery library - has lots of things you can do
-	});
-
-	$(window).on('resize', function(){
-		var windowWidth = $(window).width();
-		var windowHeight = $(window).height();
-		// this is checking for the width of the window, so that on resize, we will be checking for when the icon/nav is hidden.
-		// if the width is greater than a certain number, we need to show/hide the nav - because it's gotten lost based on slideToggle
-		console.log(windowWidth);
-		console.log(windowHeight);
-		// this is being printed in the Console
-
-
-		// this is showing or hiding each element depending on whether the viewport is above or below a certain width
-		if ( windowWidth > 200 ) {
-			$('.site-nav ul').show();
-			$('.nav-icon').hide();
-		}	else {
-			$('.site-nav ul').hide();
-			$('.nav-icon').show();
-		}
-	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-// var controller = new ScrollMagic.Controller();
-// var scene = new ScrollMagic.Scene({
-//   offset: 100, // start scene after scrolling for 100px
-//   duration: 400 // pin the element for 400px of scrolling
-// });
-
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: panel4, // starting scene, when reaching this element
-//   duration: 400 // pin the element for a total of 400px
-// })
-// .setPin(panel4); // the element we want to pin
-
-// // Add Scene to ScrollMagic Controller
-// controller.addScene(scene);
 
 
 var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
   triggerElement: trigger1,
   duration: "0%",
-  triggerHook: 0,
+  triggerHook: -10,
   reverse: true,
 })
 .setClassToggle('#navslide1', 'active')
